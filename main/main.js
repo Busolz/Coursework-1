9a2aecedf939f7f2d92f6a97d119b450a66c46d9
git config --global user.name <adeoyeoluwabusolami17@gmail.com>
2025-12-08 19:29:05 +0000

    main.js comments

--- main/main.js
+++ main/main.js
@@ -1,3 +1,5 @@
+// Import Vue createApp from global Vue object (CDN build exposes Vue globally)
 const { createApp } = Vue;
 
+// Create the app using the `App` component defined in app.js and mount to #app
 createApp(App).mount('#app');
